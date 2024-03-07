bd1a3e601f7e50d2f7d4bc5e8edd1fbe4cad70a1
DawnSingularity <70613207+DawnSingularity@users.noreply.github.com>
2024-03-07 23:46:23 +0800

    step 1, category, of posting

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import{
     useForm
 } from 'react-hook-form'
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
@@ -19,6 +20,7 @@ import Button from '../Button';
 
 const RegisterModal = () =>{
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
     const {
         register,
@@ -47,6 +49,11 @@ const RegisterModal = () =>{
             setIsLoading(false);
         })
     }
+
+    const toggle = useCallback(()=>{
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [loginModal,registerModal]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -111,7 +118,7 @@ const RegisterModal = () =>{
                         already have an account?
                     </div>
                     <div 
-                        onClick={registerModal.onClose}
+                        onClick={toggle}
                         className="text-neutral-800 cursor-pointer hover:underline"
                     >
                         Login
